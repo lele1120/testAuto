1bb584277abaa9b25625357ef5d18e3d9659b79f
xuchen <357072695@qq.com>
2018-12-27 13:54:50 +0800

    添加职业后删除

--- test_case/test_bicai.py
+++ test_case/test_bicai.py
@@ -67,40 +67,27 @@ def test_login_02(d):
     with allure.step("点击app首页一键登录"):
         click_element(d, "首页一键登录")
 
-    # time.sleep(5)
-
     with allure.step("在登录页账号输入框输入账号"):
         input_element(d, "登录页账号输入框", USER_ID)
 
-    # time.sleep(5)
-
     with allure.step("点击获取验证码"):
         click_element(d, "登录页获取验证码按钮")  # 点击获取验证码
-
-    time.sleep(5)
 
     #  如果弹出4位数字图片验证码 此处需加if判断
     with allure.step("输入4位验证码"):
         input_element(d, "图片验证码输入框", picture_verification_code )
 
-    # time.sleep(3)
-
     with allure.step("点击确认按钮"):
         click_element(d, "图片验证码确定按钮")
-
-    # time.sleep(3)
 
     with allure.step("输入6位验证码"):
         input_element(d, "登录验证码输入框", login_verification_code)
 
-    # time.sleep(3)
     with allure.step("点击立即登录"):
         click_element(d, "立即登录按钮")
 
     with allure.step("验证是否登录成功"):
         assert not d(resourceId=get_value("首页一键登录")).exists
-
-    # time.sleep(3)
 
     display_picture(d, "app首页已登录")
 
@@ -121,8 +108,6 @@ def test_sidebar_eject_03(d):
     with allure.step("点击左上角图标"):
         click_element(d, "首页左上角图标")
 
-        # time.sleep(3)
-
     with allure.step("检验侧边栏控件"):
         for i in range(cebian_button.__len__()):
             assert d(text=cebian_button[i]).exists  #验证侧边栏4个按钮控件存在
@@ -133,8 +118,6 @@ def test_sidebar_eject_03(d):
 
     display_picture(d, "弹出侧边栏")
 
-    # time.sleep(3)
-
 
 @allure.feature("04.点击侧边栏目logo")
 @allure.severity('Critical')
@@ -142,20 +125,20 @@ def test_logo_click_04(d):
     with allure.step("侧边栏logo点击"):
         click_element(d, "侧边栏logo")
 
-    # time.sleep(3)
-
     with allure.step("验证是否跳转个人资料页"):
 
         assert_title(d, "个人资料")  # 验证跳转个人资料页成功
 
     personal_data = ["性别", "微信", "职业", "实名认证", "手机号", "所在地", "个性签名"]
 
+    global Real_Name_Authentication  # 实名认证状态
+
+    Real_Name_Authentication = d(resourceId=get_value("实名认证状态")).get_text()
+
     for i in range(personal_data.__len__()):
         assert d(text=personal_data[i]).exists  # 验证个人资料内内容是否存在
 
     display_picture(d, "个人资料")
-
-    # time.sleep(3)
 
 
 @allure.feature("05.点击昵称进入修改页")
@@ -167,8 +150,6 @@ def test_nickname_click_05(d):
     with allure.step("验证修改昵称页title"):
         assert_title(d, "修改昵称")  # 验证是否跳转成功
 
-    # time.sleep(3)
-
     display_picture(d, "修改昵称页")
 
 
@@ -176,17 +157,11 @@ def test_nickname_click_05(d):
 @allure.severity('Critical')
 def test_complete_click_06(d):
 
-    # time.sleep(3)
-
     with allure.step("修改昵称为Alex"):
         input_element(d, "昵称文本框", "Alex")
 
-    # time.sleep(3)
-
     with allure.step("点击完成按钮返回个人资料页"):
         click_element(d, "完成按钮")
-
-    # time.sleep(3)
 
     with allure.step("验证是否跳转个人资料页"):
         assert_title(d, "个人资料")  # 验证跳转个人资料页成功
@@ -194,15 +169,13 @@ def test_complete_click_06(d):
     with allure.step("验证昵称是否修改成功"):
         assert d(resourceId=get_value("个人资料昵称")).get_text() == "Alex"
 
-    # time.sleep(3)
-
     display_picture(d, "点击完成页面跳转修改昵称页")
 
     # 恢复数据
     click_element(d, "个人资料昵称")
-    # time.sleep(1)
+
     input_element(d, "昵称文本框", USER_ID.replace((USER_ID[3:7]), "****"))
-    # time.sleep(1)
+
     click_element(d, "完成按钮")
 
 
@@ -215,17 +188,11 @@ def test_nickname_icon_click_07(d):
     with allure.step("验证修改昵称页是否跳转成功"):
         assert_title(d, "修改昵称")  # 验证是否跳转成功
 
-    # time.sleep(3)
-
     with allure.step("修改昵称为Alex"):
         input_element(d, "昵称文本框", "Alex")
 
-    # time.sleep(3)
-
     with allure.step("点击修改昵称页返回icon"):
         click_element(d, "返回icon")
-
-    # time.sleep(3)
 
     with allure.step("验证是否跳转个人资料页"):
         assert_title(d, "个人资料")  # 验证跳转个人资料页成功
@@ -244,8 +211,6 @@ def test_modify_sex_08(d):
 
         click_element(d, "性别")
 
-    # time.sleep(3)
-
     with allure.step("修改性别"):
         if sex_text == "男":
             click_element(d, "选项女")
@@ -253,8 +218,6 @@ def test_modify_sex_08(d):
             click_element(d, "选项男")
         else:
             print("无此选项")
-
-    time.sleep(3)
 
     with allure.step("验证性别修改是否成功"):
         modify_sex_text = d(resourceId=get_value("性别文本")).get_text()
@@ -369,95 +332,6 @@ def test_modify_profession_clear_11(d):
         display_picture(d, "职业修改页输入后删除")
 
     click_element(d, "返回icon")
-
-
-
-
-# @allure.story("验证侧边栏功能_个人资料")
-# def test_cebian_function_realname_status_02(d):
-#     """
-#         验证侧边栏功能_个人资料
-#     """
-#     with allure.step("点击是侧边栏logo"):
-#         d(resourceId=get_value("侧边栏目logo")).click(timeout=2)
-#
-#         realname_status = d(resourceId=get_value("个人资料实名认证")).get_text()
-#
-#     with allure.step("点击返回icon"):
-#         d(resourceId=get_value("返回icon")).click(timeout=2)
-#
-#
-# @allure.story("验证侧边栏功能_用户是否实名")
-# def test_cebian_function_authentication_03(d):
-#     """
-#      验证侧边栏功能_用户是否实名
-#     """
-#
-#     with allure.step("点击是否实名"):
-#         user_id = d(resourceId=get_value("是否实名")).get_text()
-#         d(resourceId=get_value("是否实名")).click(timeout=2)
-#
-#     with allure.step("点击返回icon"):
-#         d(resourceId=get_value("返回icon")).click(timeout=2)
-#
-#     with allure.step("点击是否绑卡"):
-#         user_id = d(resourceId=get_value("是否绑卡")).get_text()
-#         d(resourceId=get_value("是否绑卡")).click(timeout=2)
-#
-#     with allure.step("点击返回icon"):
-#         d(resourceId=get_value("返回icon")).click(timeout=2)
-#
-#
-# @allure.story("验证侧边栏功能_功能键_我的消息跳转")
-# def test_cebian_function_first_key_04(d):
-#     """
-#      验证侧边栏功能_功能键_我的消息跳转
-#     """
-#
-#     for i in range(cebian_button.__len__()):
-#         with allure.step("点击"+cebian_button[i]):
-#             d(text=cebian_button[i]).click(timeout=3)
-#         with allure.step("验证点击"+cebian_button[i]+"能否跳转"):
-#             time.sleep(5)
-#             title = d(resourceId=get_value("个人资料标题")).get_text()
-#
-#             if i == 0:
-#                 try:
-#                     assert title == (cebian_button[i])[2:4]
-#                     picture_name = cebian_button[i]
-#                     pictor_url = save_picture(d, picture_name)
-#                     file = open(pictor_url, 'rb').read()
-#                     allure.attach(picture_name, file, allure.attach_type.PNG)  # attach显示图片
-#                 except Exception as msg:
-#                     picture_name = cebian_button[i]
-#                     pictor_url = save_picture(d, picture_name)
-#                     file = open(pictor_url, 'rb').read()
-#                     allure.attach(picture_name, file, allure.attach_type.PNG)  # attach显示图片
-#                     print(msg)
-#             else:
-#                 try:
-#                     assert title == cebian_button[i]
-#                     picture_name = cebian_button[i]
-#                     pictor_url = save_picture(d, picture_name)
-#                     file = open(pictor_url, 'rb').read()
-#                     allure.attach(picture_name, file, allure.attach_type.PNG)  # attach显示图片
-#                 except Exception as msg:
-#                     picture_name = sys._getframe().f_code.co_name + str[i]
-#                     pictor_url = save_picture(d, picture_name)
-#                     file = open(pictor_url, 'rb').read()
-#                     allure.attach(picture_name, file, allure.attach_type.PNG)  # attach显示图片、
-#                     print(msg)
-#
-#         with allure.step("点击返回icon"):
-#             d(resourceId=get_value("返回icon")).click(timeout=1)
-
-    # time.sleep(1)
-    # picture_name = sys._getframe().f_code.co_name
-    # pictor_url = save_picture(d, picture_name)
-    # file = open(pictor_url, 'rb').read()
-    # with allure.step("点击头像"):
-    #     allure.attach(picture_name, file, allure.attach_type.PNG)  # attach显示图片
-    #     assert 1 == 1
 
 
 def click_element(d, element_name):
