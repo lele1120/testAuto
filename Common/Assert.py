2e91082c5f0da9c577778e2cff7d2570e76313f2
xuchen <357072695@qq.com>
2019-01-18 14:05:09 +0800

    调整项目结构

---
+++ Common/Assert.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+# @File    : Assert.py
+
+
+"""
+封装Assert方法
+
+"""
+import time
+
+from Common import Log, Operate
+from Common import Consts
+from Params.params import get_value
+
+
+class Assertions:
+    def __init__(self):
+        self.log = Log.MyLog()
+        self.operation = Operate.Operation()
+
+    def assert_equal_save_picture(self, d, first, second, picture_name):
+        """
+        字符串断言，成功截图展示图片，失败截图展示图片
+        :param d:d
+        :param str_a:字符串a
+        :param str_b:字符串b
+        :param picture_name:图片名称
+        :return:
+        """
+        try:
+            assert first == second
+            # self.operation.display_picture(d, picture_name + "_成功")
+            print(picture_name + "_成功")
+            return True
+        except:
+            print(picture_name + "_失败")
+            self.operation.display_picture(d, picture_name + "_失败")
+            self.log.error("断言失败 %s 不等于 %s " % (first, second))
+            Consts.RESULT_FAIL_LIST.append('fail')
+            raise
+
+    def assert_element_exists_save_picture(self, d, bool_a, picture_name):
+        """
+
+        :param d:d
+        :param bool_a: 控件.exists 存在返回ture，不存在返回Flase
+        :param picture_name: 图片名称
+        :return:
+        """
+        try:
+            assert bool_a
+            # self.operation.display_picture(d, picture_name + "_成功")
+            print(picture_name + "_成功")
+        except:
+            print(picture_name + "_失败")
+            self.operation.display_picture(d, picture_name + "_失败")
+            self.log.error("断言失败控件不存在 ")
+            Consts.RESULT_FAIL_LIST.append('fail')
+            raise
+
+    def assert_title(self, d, title):
+        """
+        :param d: 控件默认为d
+        :param title: 页面标题
+        :return: 无
+        验证页面是否跳转成功
+
+        """
+        if not d(resourceId=get_value("标题")).exists:
+            time.sleep(2)
+        else:
+            self.assert_equal_save_picture(d, title, d(resourceId=get_value("标题")).get_text(), "标题对比")
+            print("页面title为:" + str(d(resourceId=get_value("标题")).get_text()))
+            print("预期页面的title为:" + str(title))
+            time.sleep(1)
+
---
+++ Common/Operate.py
@@ -0,0 +1,72 @@
+import os
+import re
+import time
+import allure
+from Conf.Config import Config
+from Params.params import get_value
+
+
+
+class Operation:
+    def __init__(self):
+        self.config = Config()
+        self.run_path = Config.path_dir
+
+    def save_picture(self, d, picture_name):
+        """
+        保存截图
+        :param d: 默认为d
+        :param picture_name: 图片名称
+        :return:
+        """
+        picture_url = self.run_path + "/Report/picture/" + picture_name + ".png"
+        d.screenshot(picture_url)
+        return picture_url
+
+    def display_picture(self, d, picture_name):
+        """
+        测试报告展示截图
+        :param d: 控件名称，默认为d
+        :param picture_name: 图片名称
+        :return: 无
+        """
+        pictor_url = self.save_picture(d, picture_name)
+        file = open(pictor_url, 'rb').read()
+        allure.attach(picture_name, file, allure.attach_type.PNG)  # attach显示图片
+
+    def input_element(self, d, element_name, input_text):
+        """
+
+        :param d: 控件默认为d
+        :param element_name: 控件名称详见yaml文件
+        :param input_text: 需要输入的内容
+        :return: 无
+        """
+        d(resourceId=get_value(element_name)).wait(timeout=10.0)
+        d(resourceId=get_value(element_name)).set_text(input_text)
+        time.sleep(1)
+
+    def click_element_with_text(self, d, element_name, element_text):
+        """
+
+        :param d:控件默认为d
+        :param element_name:控件名称详见yaml文件
+        :param element_text:控件文本
+        :return:
+        """
+        d(resourceId=get_value(element_name), text=str(element_text)).wait(timeout=10.0)
+        d(resourceId=get_value(element_name), text=str(element_text)).click()
+        time.sleep(1)
+
+    def click_element(self, d, element_name):
+        """
+        :param d: 控件默认为d
+        :param element_name: 控件名称详见yaml文件
+        :return: 无
+        封装控件点击操作
+        """
+        d(resourceId=get_value(element_name)).wait(timeout=10.0)
+        if not d(resourceId=get_value(element_name)).exists:
+            self.display_picture(d, "控件未获取到")
+        d(resourceId=get_value(element_name)).click()
+        time.sleep(1)
